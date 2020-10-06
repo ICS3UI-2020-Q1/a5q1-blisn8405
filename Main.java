6a931eef3bc852b0bf38d3c1689d71c025087226
blisn8405 <blisn8405@wrdsb.ca>
2020-10-06 13:45:07 +0000

    q1

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner; 
 /**
  *
  * @author 
@@ -9,7 +10,15 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for user input
+Scanner input = new Scanner(System.in);
+//get users number
+System.out.println("Please enter an integer to create a multiplication table for");
+   int number = input.nextInt(); 
+
+for(int i = 0; i <=12; i++){
+   int answer= i * number;
+ System.out.println(i + " X " + number + "=" + answer);
+}
   }
 }
